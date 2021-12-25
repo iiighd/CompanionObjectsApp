bf8a1049980da9eb7de81c60fb443248057c31f3
Win10 <iiighd19@gmail.com>
2021-12-25 15:20:15 +0300

    add more picture and city

--- app/src/main/java/com/example/compapp/DayNightGround.kt
+++ app/src/main/java/com/example/compapp/DayNightGround.kt
@@ -7,22 +7,75 @@ import android.widget.Toast
 class DayNightGround (private val context: Context) {
 
     companion object {
-        const val dayBackground = R.drawable.day
-        const val nightBackground = R.drawable.night
+        //city
+        const val riyadhDBackground = R.drawable.day
+        const val meccahDBackground = R.drawable.mday
+        const val khobarDBackground = R.drawable.kday
+        const val riyadhNBackground = R.drawable.night
+        const val meccahNBackground = R.drawable.mnight
+        const val khobarNBackground = R.drawable.knight
+
+//        const val dayBackground = R.drawable.day
+//        const val nightBackground = R.drawable.night
     }
 
-    fun set(layout: View, dayNight: String) {
-        when (dayNight){
-            "day" -> {
-                layout.setBackgroundResource(dayBackground)
+    fun set(layout: View, dayNight: String , city : String ) {
+        when (city){
+            "riyadh" -> {
+                when (dayNight){
+                    "day" -> {
+                        layout.setBackgroundResource(riyadhDBackground)
+                    }
+                    "night" -> {
+                        layout.setBackgroundResource(riyadhNBackground)
+                    }
+                    else -> {
+                        Toast.makeText(context, "Please enter either day or night", Toast.LENGTH_SHORT).show()
+                    }
+                }
             }
-            "night" -> {
-                layout.setBackgroundResource(nightBackground)
+            "meccah" -> {
+                when (dayNight){
+                    "day" -> {
+                        layout.setBackgroundResource(meccahDBackground)
+                    }
+                    "night" -> {
+                        layout.setBackgroundResource(meccahNBackground)
+                    }
+                    else -> {
+                        Toast.makeText(context, "Please enter either day or night", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+            "khobar" -> {
+                when (dayNight){
+                    "day" -> {
+                        layout.setBackgroundResource(khobarDBackground)
+                    }
+                    "night" -> {
+                        layout.setBackgroundResource(khobarNBackground)
+                    }
+                    else -> {
+                        Toast.makeText(context, "Please enter either day or night", Toast.LENGTH_SHORT).show()
+                    }
+                }
             }
             else -> {
-                Toast.makeText(context, "Please enter either day or night", Toast.LENGTH_SHORT).show()
+                Toast.makeText(context, "Please full up the Text area !", Toast.LENGTH_SHORT).show()
             }
         }
+        //
+//        when (dayNight){
+//            "day" -> {
+//                layout.setBackgroundResource(dayBackground)
+//            }
+//            "night" -> {
+//                layout.setBackgroundResource(nightBackground)
+//            }
+//            else -> {
+//                Toast.makeText(context, "Please enter either day or night", Toast.LENGTH_SHORT).show()
+//            }
+//        }
     }
 
 }
--- app/src/main/java/com/example/compapp/MainActivity.kt
+++ app/src/main/java/com/example/compapp/MainActivity.kt
@@ -13,6 +13,7 @@ class MainActivity : AppCompatActivity() {
     lateinit var constraintLayout: ConstraintLayout
     lateinit var labelTextView: TextView
     lateinit var dayNightEditText: EditText
+    lateinit var cityEditText: EditText
     lateinit var changerButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,10 +22,13 @@ class MainActivity : AppCompatActivity() {
         constraintLayout = findViewById(R.id.cL)
         labelTextView = findViewById(R.id.labeltv)
         dayNightEditText = findViewById(R.id.daynightET)
+        cityEditText = findViewById(R.id.cityET)
         changerButton = findViewById(R.id.changerBTN)
+
         changerButton.setOnClickListener {
             val background = DayNightGround(this)
-            background.set(constraintLayout, dayNightEditText.text.toString())
+            background.set(constraintLayout, dayNightEditText.text.toString(), cityEditText.text.toString())
+//            background.set(constraintLayout, cityEditText.text.toString())
         }
     }
 }
